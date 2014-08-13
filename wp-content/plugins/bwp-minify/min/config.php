d1e5e92ae6ed101ff560930bf36436d5f0514e89
Arie <prasetyo.mailbox@gmail.com>
2014-08-13 10:26:19 -0400

    update

--- wp-content/plugins/bwp-minify/min/config.php
+++ wp-content/plugins/bwp-minify/min/config.php
@@ -6,11 +6,11 @@ $min_allowDebugFlag = false;
 $min_cachePath = dirname(dirname(__FILE__)) . '/cache';
 $min_documentRoot = '';
 $min_cacheFileLocking = true;
-$min_serveOptions['bubbleCssImports'] = false;
+$min_serveOptions['bubbleCssImports'] = true;
 $min_serveOptions['maxAge'] = 172800;
 $min_serveOptions['minApp']['groupsOnly'] = false;
 $min_symlinks = array();
 $min_uploaderHoursBehind = 0;
 $min_libPath = dirname(__FILE__) . '/lib';
 ini_set('zlib.output_compression', '0');
-// auto-generated on 2014-08-13 20:04:56
+// auto-generated on 2014-08-13 20:22:33
