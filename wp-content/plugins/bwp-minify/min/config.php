2551c1cba45f0fbbc76fda004e20d4a060fc7167
Arie <prasetyo.mailbox@gmail.com>
2014-08-13 09:08:15 -0400

    init

--- wp-content/plugins/bwp-minify/min/config.php
+++ wp-content/plugins/bwp-minify/min/config.php
@@ -13,4 +13,4 @@ $min_symlinks = array();
 $min_uploaderHoursBehind = 0;
 $min_libPath = dirname(__FILE__) . '/lib';
 ini_set('zlib.output_compression', '0');
-// auto-generated on 2014-08-13 06:31:59
+// auto-generated on 2014-08-13 20:04:56
