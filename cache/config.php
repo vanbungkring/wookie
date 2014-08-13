685cda0c7f086201dde059b76fc8b6733b25b1de
Arie Prasetyo <vanbungkring@Aries-MacBook-Pro.local>
2014-08-13 20:14:03 +0700

    update

---
+++ cache/config.php
@@ -0,0 +1,16 @@
+<?php
+$min_enableBuilder = false;
+$min_builderPassword = 'admin';
+$min_errorLogger = false;
+$min_allowDebugFlag = false;
+$min_cachePath = dirname(dirname(__FILE__)) . '';
+$min_documentRoot = '';
+$min_cacheFileLocking = true;
+$min_serveOptions['bubbleCssImports'] = false;
+$min_serveOptions['maxAge'] = 172800;
+$min_serveOptions['minApp']['groupsOnly'] = false;
+$min_symlinks = array();
+$min_uploaderHoursBehind = 0;
+$min_libPath = dirname(__FILE__) . '/lib';
+ini_set('zlib.output_compression', '0');
+// auto-generated on 2014-08-13 20:04:56
