d17462c499df4f1d3262ef6556468afca5de3178
Arie Prasetyo <vanbungkring@Aries-MacBook-Pro.local>
2014-08-13 20:19:48 +0700

    update cache

--- cache/config.php
+++ cache/config.php
@@ -3,14 +3,14 @@ $min_enableBuilder = false;
 $min_builderPassword = 'admin';
 $min_errorLogger = false;
 $min_allowDebugFlag = false;
-$min_cachePath = dirname(dirname(__FILE__)) . '';
+$min_cachePath = '/var/www/teknoboost.com/wp-content/plugins/bwp-minify/cache';
 $min_documentRoot = '';
 $min_cacheFileLocking = true;
-$min_serveOptions['bubbleCssImports'] = false;
+$min_serveOptions['bubbleCssImports'] = true;
 $min_serveOptions['maxAge'] = 172800;
 $min_serveOptions['minApp']['groupsOnly'] = false;
 $min_symlinks = array();
 $min_uploaderHoursBehind = 0;
 $min_libPath = dirname(__FILE__) . '/lib';
 ini_set('zlib.output_compression', '0');
-// auto-generated on 2014-08-13 20:04:56
+// auto-generated on 2014-08-13 20:15:49
